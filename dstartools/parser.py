197c98d99c47d4cb6be187b38712efab7a169cd8
Delyan Raychev <delyanr@gmail.com>
2015-12-24 15:41:56 -0800

    Refactor parser

--- dstartools/parser.py
+++ dstartools/parser.py
@@ -1,85 +1,84 @@
 #!/usr/bin/env python
 
+import enum
 import struct
 import zlib
 
 from .colors import Colors
 
-DRATS_HEAD = '[SOB]'
-DRATS_TAIL = '[EOB]'
-drats_head_len = len(DRATS_HEAD)
-drats_tail_len = len(DRATS_TAIL)
 
+class MessageType(enum.Enum):
+    drats = 'drats'
+    dprs = 'dprs'
 
-DPRS_HEAD = '$GPGGA'
-DPRS_TAIL = '\n'
-dprs_head_len = len(DPRS_HEAD)
-dprs_tail_len = len(DPRS_TAIL)
+# type: head, tail
+BOOKENDS = {
+    MessageType.drats: ('[SOB]', '[EOB]'),
+    MessageType.dprs: ('$GPGGA', '\n'),
+}
 
 
 class Parser(object):
     @classmethod
-    def _parse_drats_message(cls, packet):
-        msg = {'raw_data': packet}
-
-        if ord(packet[0]) not in (0xDD, 0x22):
-            return {'status': 'error', 'message': None}
-
-        # add data
-        if ord(packet[0]) == 0xDD:
-            msg['is_compressed'] = True
-            msg['data'] = zlib.decompress(packet[25:])
-        else:
-            msg['is_compressed'] = False
-            msg['data'] = packet[25:]
-
-        # unpack header
-        ordered_value_names = ('magic_number',
-                               'sequence',
-                               'session',
-                               'message_type',
-                               'checksum',
-                               'length',
-                               'source',
-                               'destination')
-
-        STRUCT_FORMAT = "!BHBBHH8s8s"
-        msg.update(dict(zip(ordered_value_names,
-                            struct.unpack(STRUCT_FORMAT, packet[:25]))))
-
-        # TODO(delyan): CRC calculations and comparison here
-
-        for k, v in msg.iteritems():
-            print '{}{}:  {}{}{}'.format(Colors.YELLOW, k, Colors.BLUE, v, Colors.RESET)
-
-        return {'status': 'ok', 'message': msg}
-
-    @classmethod
-    def _is_valid_drats(cls, buff):
-        return (buff and len(buff) > drats_head_len + drats_tail_len and
-                DRATS_HEAD in buff and
-                DRATS_TAIL in buff[buff.index(DRATS_HEAD):])
-
-    @classmethod
     def search_drats_message(cls, buff):
-        if cls._is_valid_drats(buff):
-            begin = buff.index(DRATS_HEAD) + drats_head_len
-            end = begin + buff[begin:].index(DRATS_TAIL)
-            message = cls._parse_drats_message(buff[begin:end])
-            return (message, buff[:begin - drats_head_len] + buff[end + drats_tail_len:])
-        return (None, buff)
-
-    @classmethod
-    def _is_valid_dprs(cls, buff):
-        return (buff and len(buff) > dprs_head_len + dprs_tail_len and
-                DPRS_HEAD in buff and
-                DPRS_TAIL in buff[buff.index(DPRS_HEAD):])
+        head, tail = BOOKENDS.get(MessageType.drats)
+        if not _is_valid(MessageType.drats, buff):
+            return (None, buff)
+        start = buff.index(head) + len(head)
+        end = start + buff[start:].index(tail)
+        message = _parse_drats_message(buff[start:end])
+        return (message, buff[:start - len(head)] + buff[end + len(tail):])
 
     @classmethod
     def search_dprs_message(cls, buff):
-        if cls._is_valid_dprs(buff):
-            begin = buff.index(DPRS_HEAD)
-            end = begin + buff[begin:].index(DPRS_TAIL)
-            message = buff[begin:end]
-            return (message, buff[:begin] + buff[end + dprs_tail_len:])
-        return (None, buff)
+        head, tail = BOOKENDS.get(MessageType.dprs)
+        if not _is_valid(MessageType.dprs, buff):
+            return (None, buff)
+        start = buff.index(head)
+        end = start + buff[start:].index(tail)
+        message = buff[start:end]
+        return (message, buff[:start] + buff[end + len(tail):])
+
+
+def _is_valid(msg_type, buff):
+    head, tail = BOOKENDS.get(msg_type)
+    return (buff and
+            len(buff) > len(head) + len(tail) and
+            head in buff and
+            tail in buff[buff.index(head):])
+
+
+def _parse_drats_message(packet):
+    msg = {'raw_data': packet}
+
+    if ord(packet[0]) not in (0xDD, 0x22):
+        return {'status': 'error', 'message': None}
+
+    # add data
+    if ord(packet[0]) == 0xDD:
+        msg['is_compressed'] = True
+        msg['data'] = zlib.decompress(packet[25:])
+    else:
+        msg['is_compressed'] = False
+        msg['data'] = packet[25:]
+
+    # unpack header
+    ordered_value_names = ('magic_number',
+                           'sequence',
+                           'session',
+                           'message_type',
+                           'checksum',
+                           'length',
+                           'source',
+                           'destination')
+
+    STRUCT_FORMAT = "!BHBBHH8s8s"
+    msg.update(dict(zip(ordered_value_names,
+                        struct.unpack(STRUCT_FORMAT, packet[:25]))))
+
+    # TODO(delyan): CRC calculations and comparison here
+
+    for k, v in msg.iteritems():
+        print '{}{}:  {}{}{}'.format(Colors.YELLOW, k, Colors.BLUE, v, Colors.RESET)
+
+    return {'status': 'ok', 'message': msg}
